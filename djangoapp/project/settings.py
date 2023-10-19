3d1525af685d4484f25e55190b1798ac4dd8fce2
Rafael R Rosa <rarorza@proton.me>
2023-10-19 19:12:25 -0300

    config: add load_dotenv in settings.py

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -13,9 +13,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR.parent / "data" / "web"
+
+# DOTENV servidor
+load_dotenv(BASE_DIR.parent / "dotenv_files" / ".env", override=True)
 
 
 # Quick-start development settings - unsuitable for production
