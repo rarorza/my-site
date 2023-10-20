28053ee26806e21784928ee4059fa3118f80a36d
Rafael R Rosa <rarorza@proton.me>
2023-10-20 10:45:12 -0300

    update

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv("DEBUG", 0)))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    host.strip() for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host.strip()
+]
 
 
 # Application definition
